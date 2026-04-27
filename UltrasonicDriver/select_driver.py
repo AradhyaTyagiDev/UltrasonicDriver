e609f5e75eadf9d5e602db5101c4ca79b31b840b
aradhya <aaradhya.me.tyagi@gmail.com>
2026-04-27 07:19:03 +0530

    USD TASK: Bug Fixes. Release build.

--- UltrasonicDriver/select_driver.py
+++ UltrasonicDriver/select_driver.py
@@ -1,8 +1,3 @@
 Import("env")
 
 name = env["PIOENV"]
-
-if "idf" in name:
-    env.Append(CPPDEFINES=["ULTRASONIC_USE_ESP_IDF"])
-elif "arduino" in name:
-    env.Append(CPPDEFINES=["ULTRASONIC_USE_ARDUINO"])
